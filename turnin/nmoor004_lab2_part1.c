aebad7d5ad2ba8ebbba463ee593c62336e76e816
nmoor004 <38025224+nmoor004@users.noreply.github.com>
2019-10-03 22:13:43 -0700

    Completed part 1

--- turnin/nmoor004_lab2_part1.c
+++ turnin/nmoor004_lab2_part1.c
@@ -18,7 +18,7 @@ int main(void) {
 	DDRB = 0xFF; PORTB = 0x00;
     /* Insert your solution below */
 	unsigned char temp_val = 0x00;
-	    while (1) {
+	    while (1) { 		// Set temp_val to the bits of PINA and check if it's first nibble is 0001.
 		temp_val = PINA;
 
 		if (temp_val == 0x01) {
